e3ce9de1fb04aa57c7457595dd9fbca8e8c865e4
alina-alieks <145293161+alina-alieks@users.noreply.github.com>
2023-12-20 01:14:05 +0200

    hw3 complete

--- js/task-3.js
+++ js/task-3.js
@@ -1,20 +1,17 @@
 "use strict"
 
-function getElementWidth(content, padding, border) {
-    //2 ways resolving the task
-    //1-st
-    const boxSizing = Number.parseFloat(content) + 2 * Number.parseFloat(padding) + 2 * Number.parseFloat(border);
-    
-    //2-d
-    // content = Number.parseFloat(content);
-    // padding = Number.parseFloat(padding);
-    // border = Number.parseFloat(border);
-    // const boxSizing = content + 2 * padding + 2 * border;
-
-    return boxSizing;
-
+function filterArray(numbers, value) {
+    const array = [];
+    for (let number of numbers) {
+        if (number > value) {
+            array.push(number);
+        }
+    }
+return array
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
